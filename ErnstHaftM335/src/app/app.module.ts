7c878356f0013e328b60480b11102e55791be968
Alex Belik <alex.belik26@gmail.com>
2025-01-22 14:49:25 +0100

    task 1

--- ErnstHaftM335/src/app/app.module.ts
+++ ErnstHaftM335/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { RouteReuseStrategy } from '@angular/router'
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { TaskPageComponent } from './components/task-page/task-page.component';
-import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, TaskPageComponent, TaskComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- ErnstHaftM335/src/app/components/task1/task1.component.ts
+++ ErnstHaftM335/src/app/components/task1/task1.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {IonButton, IonContent, IonHeader, IonImg, IonTitle, IonToolbar} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-task1',
   templateUrl: './task1.component.html',
-  styleUrls: ['./task1.component.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonImg,
+    IonButton
+  ],
+  styleUrls: ['./task1.component.scss']
 })
-export class Task1Component  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class Task1Component {
+  completeTask() {
+    console.log('Task 1 completed!');
+  }
 }
