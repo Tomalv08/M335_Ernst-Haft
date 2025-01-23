600e0217a4bc3616f1a3715cd03673483b062824
Tomas TeixeiraAlves <tomas_Teixeiraalves@sluz.ch>
2025-01-23 13:28:15 +0100

    adding task3 view

--- ErnstHaftM335/src/app/components/task3/task3.component.ts
+++ ErnstHaftM335/src/app/components/task3/task3.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {DecimalPipe} from "@angular/common";
+import {IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonImg} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-task3',
   templateUrl: './task3.component.html',
   styleUrls: ['./task3.component.scss'],
+  imports: [
+    DecimalPipe,
+    IonButton,
+    IonImg,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent
+  ]
 })
 export class Task3Component  implements OnInit {
   @Input() moveToNextTask!: () => void;
