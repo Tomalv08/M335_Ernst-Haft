5649a87561890eac400a6b1a1780d9f36ad6f8de
Alex Belik <alex.belik26@gmail.com>
2025-01-23 12:59:40 +0100

    task 4

--- ErnstHaftM335/src/app/components/task4/task4.component.ts
+++ ErnstHaftM335/src/app/components/task4/task4.component.ts
@@ -1,15 +1,57 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Device } from '@capacitor/device';
+import { AlertController } from '@ionic/angular';
+import {
+  IonButton,
+  IonCard, IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar
+} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-task4',
   templateUrl: './task4.component.html',
   styleUrls: ['./task4.component.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonButton
+  ]
 })
-export class Task4Component  implements OnInit {
+export class Task4Component implements OnInit {
   @Input() moveToNextTask!: () => void;
+  isCharging: boolean = false;
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.checkBatteryStatus();
+  }
 
+  async checkBatteryStatus() {
+    const info = await Device.getBatteryInfo();
+    this.isCharging = info.isCharging ?? false;
+  }
+
+  async connectToCharger() {
+    // Simulate connecting to a charger
+    this.isCharging = true;
+    const alert = await this.alertController.create({
+      header: 'Task Completed',
+      message: 'You have successfully connected to the charger.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
